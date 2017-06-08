a4734f21b48359d5757a36ecd5719a75330e84ec
thechutrain <chu.chuyalan@gmail.com>
2017-06-08 17:51:21 -0400

    cleaning up repo, adding in redux, react-redux, redux-logger npm

--- budget-tracker/src/index.js
+++ budget-tracker/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-// import registerServiceWorker from './registerServiceWorker';
-import './index.css'
+import store from './store'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 // registerServiceWorker();
